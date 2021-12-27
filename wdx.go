7944661c17bbb5f4ad3c41c24cb8d105aaf68a50
ranajahanzaib <iam@ranajahanzaib.com>
2021-12-28 00:09:45 +0500

    Add ScrapLinks Function

--- wdx.go
+++ wdx.go
@@ -9,9 +9,25 @@ package wdx
 
 import (
 	"fmt"
+
+	"github.com/gocolly/colly"
 )
 
 // Log progress / details on the go
 func Log(format string, args ...interface{}) {
 	fmt.Printf(format, args...)
+}
+
+// Scrap links from a given URL using class name, and return the links
+func ScrapLinks(url string, className string) []string {
+	var links []string
+	c := colly.NewCollector(
+		colly.AllowedDomains("en.wikipedia.org"),
+	)
+
+	c.OnHTML(className, func(e *colly.HTMLElement) {
+		links = e.ChildAttrs("a", "href")
+	})
+	c.Visit(url)
+	return links
 }
